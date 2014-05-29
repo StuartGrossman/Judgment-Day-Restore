a3d5b2246de39fff3b496dc84f3ee4fd6aad0e01
shane <shanemiller500@gmail.com>
2014-05-29 14:36:26 -0700

    updated search bar

--- app/controllers/linkedin_api_controller.rb
+++ app/controllers/linkedin_api_controller.rb
@@ -25,31 +25,6 @@ class LinkedinApiController < ApplicationController
     def show
     	
     end
-    def to_mash
-      			::Hashie::Mash.new(self)
-    	end
-
-    		# Converts a mash back to a hash (with stringified or symbolized keys)
-    	def to_hash(options = {})
-      out = {}
-      keys.each do |k|
-        assignment_key = if options[:stringify_keys]
-                           k.to_s
-                         elsif options[:symbolize_keys]
-                           k.to_s.to_sym
-                         else
-                           k
-                         end
-        if self[k].is_a?(Array)
-          out[assignment_key] ||= []
-          self[k].each do |array_object|
-            out[assignment_key] << (Hash === array_object ? flexibly_convert_to_hash(array_object, options) : array_object)
-          end
-        else
-          out[assignment_key] = Hash === self[k] ? flexibly_convert_to_hash(self[k], options) : self[k]
-        end
-      end
-      out
-    end
+   
 end
 
