4778b4441bf5868f5f4d65e1cdc7b6fdaa2e2a21
shane <shanemiller500@gmail.com>
2014-05-29 16:16:50 -0700

    update

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,11 +1,13 @@
 class HomePageController < ApplicationController
 	  def search
+      @companies = Company.where(universal_name: params[:q])
     @search = SimpleSearch.new SimpleSearch.get_params(params)
    
     if @search.valid?
       @companies = @search.search_within Company.all, :title
     else
       flash[:errors]=@search.errors.full_messages
+
     end
   end
 end
